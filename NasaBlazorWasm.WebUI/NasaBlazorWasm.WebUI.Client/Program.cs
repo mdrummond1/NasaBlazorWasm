15ca156180c0c80a871b17fd473d8373d326b985
Matthew Drummond <matthew.k.drummond1337@gmail.com>
2025-05-10 19:04:37 -0500

    Delete NasaBlazorWasm.WebUI directory

--- NasaBlazorWasm.WebUI/NasaBlazorWasm.WebUI.Client/Program.cs
+++
@@ -1,7 +0,0 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-builder.Services.AddHttpClient();
-
-await builder.Build().RunAsync();
